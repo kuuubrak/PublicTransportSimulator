1f909453c47f051aef8b56c50adb9044c5e81991
maciej168 <>
2014-03-23 05:22:58 +0100

    Comparability

--- src/Order/OrderPrioritableAbstract.java
+++ src/Order/OrderPrioritableAbstract.java
@@ -1,11 +1,15 @@
 
 package Order;
 
+import java.io.Serializable;
+
 /**
  * Częściowa implementacja Rozkazu. Część dot. priorytetów.
  * @author Maciej Majewski
+ * @param <T> interfejs funkcjonalny
  */
-public abstract class OrderPrioritableAbstract<T> implements Order<T>{
+public abstract class OrderPrioritableAbstract<T> implements    Serializable,
+                                                                Order<T> {
 
     @Override
     public int compareTo(Order<T> o) {
