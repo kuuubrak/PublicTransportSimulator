766e565b09ab2a1b7837a0c496c8001859afedb3
maciej168 <>
2014-03-23 04:58:23 +0100

    Dla modułów wykonujących rozkazy, częściowa implementacja

---
+++ src/Order/OrderRecipientAbstract.java
@@ -0,0 +1,19 @@
+package Order;
+
+import java.util.logging.Logger;
+
+/**
+ * Jak komuś się nie chce tego pisać, to może odziedziczyć.
+ * @author Maciej Majewski
+ * @param <T> interfejs funkcjonalny.
+ * TODO Przetestować.
+ */
+public abstract class OrderRecipientAbstract<T> implements OrderRecipient<T> {
+    @Override
+    public void executeOrder(Order<T> toExec){
+        Logger.getLogger(OrderRecipient.class.getName(),"Executing order of type: " + toExec.getClass().getName());
+        toExec.execute((T)this); // W sumie nie testowałem czy ta linijka zadziała.
+                                //W sumie powinna, chyba że interf. funkcjonalny T nie będzie implementowany
+                                //przez dziedzica.
+    }
+}
