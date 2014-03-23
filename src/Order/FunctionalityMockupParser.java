660e1ccf26efec5060a72179cef08e89b453597e
maciej168 <>
2014-03-23 05:23:39 +0100

    Podstawowa funkcjonalność zkm i gui

---
+++ src/Order/FunctionalityMockupParser.java
@@ -0,0 +1,18 @@
+
+package Order;
+
+import DataModel.Mockup;
+
+/**
+ * Zbiór funkconalności oczekiwanych od obiektów mających
+ * 'rozumieć' makiety Pana Daniela
+ * @author Maciej Majewski
+ */
+public interface FunctionalityMockupParser {
+    
+    /**
+     * Pozostaje przetworzyć otrzymaną makietę.
+     * @param fresh makieta którą dostajesz
+     */
+    public void newMockup(Mockup fresh);
+}
