1feaf9dca11af349e9c2d32edc4fe3cb71c0955d
maciej168 <>
2014-03-23 04:58:36 +0100

    info

---
+++ src/Order/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Pakiet zawierający implementację idei rozkazu. Jako iż w użyciu jest trywialny,
+ * łatwo będzie go zastosować w każym module.
+ * Generyczność jest głównie coby samem nie rzutować Objectów na Order'y.
+ * Zapraszam autorów modółów do modyfikacji interfejsów 'funkcjonalnych'.
+ * @author Maciej Majewski
+ */
+package Order;
