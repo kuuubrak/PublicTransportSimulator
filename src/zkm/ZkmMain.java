a10b43a2988c7386d0f7e3ed6e0b1dbc070e54de
macdrag <macdrag@gmail.com>
2014-05-06 09:32:04 +0200

    Poprawki w zkm.

--- src/zkm/ZkmMain.java
+++ src/zkm/ZkmMain.java
@@ -6,16 +6,12 @@ import Order.Order;
 import Order.zkm.OrderReleaseBus;
 import Order.zkm.OrderTrapBus;
 import network.Client;
-import sun.util.logging.resources.logging_pt_BR;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
-
-import static java.lang.Thread.sleep;
 
 /**
- * Created by macdr_000 on 19.03.14.
+ * @author Rafal Jagielski
  */
 public class ZkmMain implements FunctionalityMockupParser{
 
@@ -25,6 +21,8 @@ public class ZkmMain implements FunctionalityMockupParser{
     private Mockup mockup;
     private Integer lowerBound;
     private Integer upperBound;
+    private Integer loopTimeMinute; // TODO
+
 
     public static void main(String[] args)
     {
@@ -32,10 +30,12 @@ public class ZkmMain implements FunctionalityMockupParser{
         //{
         //    System.out.println("Wrong number of arguments. Program is stopping.");
         //    return;
-       // }
-
+        //}
+        String host = "127.0.0.1";
+        int port = 8125;
         Integer lowerBound = 10;//Integer.parseInt(args[0]);
         Integer upperBound = 20;//Integer.parseInt(args[1]);
+        Integer loopTimeMinute = 0;
         if (lowerBound > upperBound)
         {
             System.out.println("Lower bound (first arg) cannot be higher than upper bound (second argument)\n"
@@ -43,49 +43,44 @@ public class ZkmMain implements FunctionalityMockupParser{
             return;
         }
 
-        ZkmMain zkmMain = new ZkmMain("127.0.0.1", 8125, lowerBound, upperBound);
+        ZkmMain zkmMain = new ZkmMain(host, port, lowerBound, upperBound, loopTimeMinute);
         zkmMain.mainLoop();
     }
 
-    public ZkmMain(String host, int port, Integer lowerBound, Integer upperBound)
+    public ZkmMain(String host, int port, Integer lowerBound, Integer upperBound, Integer loopTimeMinute)
     {
         this.host = host;
         this.port = port;
         this.lowerBound = lowerBound;
         this.upperBound = upperBound;
+        this.loopTimeMinute = loopTimeMinute;
         mockup = new Mockup(new ArrayList<Bus>(), new ArrayList<BusStop>()); //Tymczasowo
     }
 
     private void receiveMockup()
     {
 
-        Order<FunctionalityMockupParser> order = null;
+        Order<FunctionalityMockupParser> order;
         order = sc.getOrdersQueue().poll();
 
-        if(order != null){
+        if(order != null)
+        {
                 order.execute(this);
-            }
+        }
 
     }
 
     public void mainLoop()
     {
         sc.establishConnection(host, port);
-        Integer loopTimeMinute = 0; /* TODO */
 
         System.out.println("Press enter key to stop.");
-        Integer k = 0;
-        do {
 
-            System.out.println("K1");
-            sc.send(new OrderReleaseBus());
-            System.out.println("K2");
+        do {
             receiveMockup();
-            System.out.println("K3");
             ArrayList<Bus> buses = (ArrayList<Bus>) mockup.getBuses();
             ArrayList<BusStop> busStops = (ArrayList<BusStop>) mockup.getBusStops();
 
-            Integer busesNr = buses.size();
             Integer freeSeatsNr = 0;
             Integer peopleWaitingNr = 0;
 
@@ -99,42 +94,9 @@ public class ZkmMain implements FunctionalityMockupParser{
                 peopleWaitingNr += busStop.getNumberOfPassengersWaiting();
             }
 
-            // Decision making
-            if (freeSeatsNr == 0)
-            {
-                if(peopleWaitingNr == 0)
-                {
-                    sc.send(new OrderTrapBus());
-                }
-                else
-                {
-                    sc.send(new OrderReleaseBus());
-                }
-            }
-            else
-            {
-                Integer coef = peopleWaitingNr > freeSeatsNr ? (peopleWaitingNr / freeSeatsNr) : 1;
-                coef = coef * loopTimeMinute;
-                if (coef > upperBound)
-                {
-                    sc.send(new OrderReleaseBus());
-                }
-                else if (coef < lowerBound)
-                {
-                    sc.send(new OrderTrapBus());
-                }
-            }
+            makeDecision(freeSeatsNr, peopleWaitingNr);
 
-            try
-            {
-                k = System.in.available();
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-                return;
-            }
-        } while (k == 0);
+        } while (endLoop());
 
         System.out.println("ZKM has stopped.");
     }
@@ -142,5 +104,50 @@ public class ZkmMain implements FunctionalityMockupParser{
     @Override
     public void newMockup(Mockup fresh) {
         mockup = fresh;
+    }
+
+    private boolean endLoop()
+    {
+        try
+        {
+            if (System.in.available() != 0)
+            {
+                return false;
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    private void makeDecision(int freeSeatsNr, int peopleWaitingNr)
+    {
+        if (freeSeatsNr == 0)
+        {
+            if(peopleWaitingNr == 0)
+            {
+                sc.send(new OrderTrapBus());
+            }
+            else
+            {
+                sc.send(new OrderReleaseBus());
+            }
+        }
+        else
+        {
+            Integer coef = peopleWaitingNr > freeSeatsNr ? (peopleWaitingNr / freeSeatsNr) : 1;
+            coef = coef * loopTimeMinute;
+            if (coef > upperBound)
+            {
+                sc.send(new OrderReleaseBus());
+            }
+            else if (coef < lowerBound)
+            {
+                sc.send(new OrderTrapBus());
+            }
+        }
     }
 }
